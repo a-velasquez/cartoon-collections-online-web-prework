02e7345a2b3d9603e8d716fdb79cf3bacab51800
Alex Velasquez <jtinnesco@gmail.com>
2019-04-27 04:34:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index(1) do |dwarf, number|
+    puts "#{number}"
 end
 
 def summon_captain_planet# code an argument here
